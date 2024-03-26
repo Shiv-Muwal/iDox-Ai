66f5f25563c97481c9803a0fe1b62e6325f58097
Shiv-468 <shivmuwal.147@gmail.com>
2024-03-26 15:18:19 +0530

    responsive in progress

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,3 +37,16 @@ $('.slider_2').slick({
         }
     ]
 });
+const menuicon = document.querySelector(".menuicon");
+const menulist = document.querySelector(".menulist");
+const navLink = document.querySelectorAll(".nav_link");
+navLink.forEach(a => a.addEventListener("click", () => {
+    menulist.classList.toggle("show");
+    document.body.classList.remove("overflow-hidden");
+    menuicon.classList.toggle("show");
+}))
+menuicon.addEventListener("click", () => {
+    menulist.classList.toggle("show");
+    document.body.classList.toggle("overflow-hidden");
+    menuicon.classList.toggle("show");
+});
