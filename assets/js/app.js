da268bbf31b71e972930287448b0749b91cd895f
Shiv-468 <shivmuwal.147@gmail.com>
2024-03-27 12:41:08 +0530

    active slick slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,39 @@
+$('.slider_1').slick({
+    dots: false,
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    autoplaySpeed: 0,
+    speed: 5000,
+    pauseOnHover: false,
+    cssEase: 'linear',
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 3,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 700,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        }
+      ]
+});
 $('.slider_2').slick({
     dots: true,
     infinite: true,
@@ -8,6 +44,24 @@ $('.slider_2').slick({
     autoplay: true,
     slidesToShow: 1,
     slidesToScroll: 1,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+    centerMode:false,
+          }
+        },
+        {
+            breakpoint: 950,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+      centerMode:false,
+            }
+          },
+    ]
 });
 const menuicon = document.querySelector(".menuicon");
 const menulist = document.querySelector(".menulist");
